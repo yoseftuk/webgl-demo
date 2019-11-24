a9dc60899ca2e9dca2ca6382553f35ef09734647
yoseftuk <yoostuk@gmail.com>
2019-11-24 07:02:17 +0200

    init

---
+++ precache-manifest.8cc70e26a23dc932eddc0342415fd1c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2eeab3dd331fcd9962d4c3fc1e1ebb",
+    "url": "/webgl-demo/index.html"
+  },
+  {
+    "revision": "946904f87f863db01b92",
+    "url": "/webgl-demo/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "a5e7302364c8db3f6a8f",
+    "url": "/webgl-demo/static/js/2.a0cbadac.chunk.js"
+  },
+  {
+    "revision": "946904f87f863db01b92",
+    "url": "/webgl-demo/static/js/main.95dc91d5.chunk.js"
+  },
+  {
+    "revision": "5e95a297460f6823d722",
+    "url": "/webgl-demo/static/js/runtime~main.d40136e9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/webgl-demo/static/media/logo.ee7cd8ed.svg"
+  }
+]);
